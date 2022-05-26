5857b59faf8a39763e055857b243598963102c6e
flush36 <thiagoferreira.dev@gmail.com>
2022-05-26 15:21:54 -0300

    ativando o dom

--- src/app/shared/components/photo-frame/photo-frame.component.spec.ts
+++ src/app/shared/components/photo-frame/photo-frame.component.spec.ts
@@ -1,3 +1,4 @@
+import { element } from 'protractor';
 import { PhotoFrameModule } from './photo-frame.module';
 import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { PhotoFrameComponent } from './photo-frame.component';
@@ -44,8 +45,39 @@ describe(PhotoFrameComponent.name, () => {
       expect(times).toBe(2);
   }));
 
-  it(`Should display number of likes when (@Input likes) is incremented`, () => {
+  it(`(D) Should display number of likes when (@Input likes) is incremented`, () => {
+    fixture.detectChanges();
+    component.likes++;
+    expect(component.likes).toBe(1);
+    fixture.detectChanges();
+    const element: HTMLElement = fixture.nativeElement.querySelector('.like-counter');
+    expect(element.textContent.trim()).toBe('1');
+  });
 
+  it(`(D) Should update aria-label when (@Input likes) is incremented`, () => {
+    fixture.detectChanges();
+    component.likes++;
+    fixture.detectChanges();
+    const element: HTMLElement = fixture.nativeElement.querySelector('span');
+    expect(element.getAttribute('aria-label')).toBe('1: people liked');
+  });
+
+  it(`(D) Should have aria-label with 0 (@Input likes)`, () => {
+    fixture.detectChanges();
+    const element: HTMLElement = fixture.nativeElement.querySelector('span');
+    expect(element.getAttribute('aria-label')).toBe('0: people liked');
+  });
+
+  it(`(D) Should display image with src and description when
+    bound to properties`,() => {
+    const description = 'some description';
+    const src = 'http://somesite.com/img.jpg';
+    component.src = src;
+    component.description = description;
+    fixture.detectChanges();
+    const img: HTMLElement = fixture.nativeElement.querySelector('img');
+    expect(img.getAttribute('src')).toBe(src);
+    expect(img.getAttribute('alt')).toBe(description);
   });
 
 });
