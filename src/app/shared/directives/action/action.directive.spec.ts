b0e3f9fb5216a9e4a12e1609e3b8d62fb0d92442
Thiago Teofilo <thiago.teofilo@shift.com.br>
2022-06-01 16:34:40 -0300

    adicionando diretiva para tests

--- src/app/shared/directives/action/action.directive.spec.ts
+++ src/app/shared/directives/action/action.directive.spec.ts
@@ -1,13 +1,29 @@
 import { ActionDirectiveModule } from './action.module';
 import { TestBed } from '@angular/core/testing';
 import { ActionDirective } from './action.directive';
-describe(ActionDirective.name, () => {
+import { Component } from '@angular/core';
 
+describe(ActionDirective.name, () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      declarations: [ActionDirectiveTestComponent],
       imports: [ActionDirectiveModule]
     }).compileComponents();
   });
 
+  @Component({
+    template: `<div (appAction)="actionHandler()"></div>`
+  })
+  class ActionDirectiveTestComponent {
+    private event: Event = null;
+
+    public actionHandler(event: Event): void {
+      this.event = event;
+    }
+
+    public hasEvent(): boolean {
+      return !!this.event;
+    }
+  }
 
 });
