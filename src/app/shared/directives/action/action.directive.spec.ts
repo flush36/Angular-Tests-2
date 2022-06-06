123a474e91f5282aff6212127e541ff30ea60c70
Thiago Teofilo <thiago.teofilo@shift.com.br>
2022-06-06 16:51:07 -0300

    teste em diretiva

--- src/app/shared/directives/action/action.directive.spec.ts
+++ src/app/shared/directives/action/action.directive.spec.ts
@@ -1,18 +1,39 @@
 import { ActionDirectiveModule } from './action.module';
-import { TestBed } from '@angular/core/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { ActionDirective } from './action.directive';
 import { Component } from '@angular/core';
+import { Key } from 'protractor';
 
 describe(ActionDirective.name, () => {
+
+  let fixture: ComponentFixture<ActionDirectiveTestComponent>;
+  let component: ActionDirectiveTestComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ActionDirectiveTestComponent],
       imports: [ActionDirectiveModule]
     }).compileComponents();
+
+    fixture = TestBed.createComponent(ActionDirectiveTestComponent);
+    component = fixture.componentInstance;
   });
 
+  it(`(D) (@Output appAction) should emit event with payload when ENTER key is pressed`, () => {
+    const divEl: HTMLElement = fixture.nativeElement.querySelector('.dummy-component');
+    const event = new KeyboardEvent('keyup', { key: 'Enter'});
+    divEl.dispatchEvent(event);
+    expect(component.hasEvent()).toBe(true);
+  });
+
+  it(`(D) (@Output appAction) should emit event with payload when clicked`, () => {
+    const divEl: HTMLElement = fixture.nativeElement.querySelector('.dummy-component');
+    const event = new Event('click');
+    divEl.dispatchEvent(event);
+    expect(component.hasEvent()).toBe(true);
+  });
   @Component({
-    template: `<div (appAction)="actionHandler()"></div>`
+    template: `<div class="dummy-component" (appAction)="actionHandler($event)"></div>`
   })
   class ActionDirectiveTestComponent {
     private event: Event = null;
